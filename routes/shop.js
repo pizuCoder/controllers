13a732af81582c71046030e682505bfc316ebe9f
Priya Moghe <102720207+pizuCoder@users.noreply.github.com>
2023-05-14 13:16:15 +0530

    controllers-proj

---
+++ routes/shop.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+const express = require('express');
+const productController = require('../controllers/products')
+
+
+
+const router = express.Router();
+
+router.get('/', productController.getProduct);
+
+module.exports = router;
